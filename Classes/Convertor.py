09a85671b515f5e098b3b6eedb891d6800b0ebd6
Weliras <ptacek77711@seznam.cz>
2021-10-17 16:42:20 +0200

    Convertor class

--- Classes/Convertor.py
+++ Classes/Convertor.py
@@ -1,5 +1,8 @@
+import os
 import urllib.request
 import sys, traceback
+
+from Classes.Gene import Gene, GTF_File_Line
 from Classes.Virus import Virus
 from Classes.SamRecord import SamRecord
 
@@ -24,7 +27,64 @@ class Convertor:
         return virusSeq_to_name_map
 
     @staticmethod
-    def load_file(url, map):
+    def load_gtf_files():
+        d = 'gtf_files'
+        folders = [os.path.join(d, o) for o in os.listdir(d) if os.path.isdir(os.path.join(d, o))]
+
+        genes = []
+        for dir in folders:
+            virus_id = dir.split("\\")[1].replace("-", "|")
+            gtf_file = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))]
+            if len(gtf_file) <= 0:
+                print(f"Gtf file for virus_id: {virus_id} was not found.")
+                continue
+            gtf_file = gtf_file[0]
+            try:
+                with open(os.path.join(dir, gtf_file), "r") as file:
+                    prev_line = ""
+                    gene = Gene(virus_id)
+                    for line in file:
+                        if line.startswith("#"):
+                            continue
+                        if line.split("\t")[2] == "CDS":
+                            if prev_line == "CDS" or prev_line == "start_codon":
+                                genes.append(Gene(virus_id=gene.virus_id, CDS=gene.CDS, start_codon=gene.start_codon, stop_codon=gene.stop_codon))
+
+                            cds_line = GTF_File_Line()
+                            line_split = line.split("\t")
+                            cds_line.load_from_line(line_split)
+
+                            gene.CDS = cds_line
+
+                            prev_line = "CDS"
+                        elif line.split("\t")[2] == "start_codon":
+
+                            start_codon_line = GTF_File_Line()
+                            line_split = line.split("\t")
+                            start_codon_line.load_from_line(line_split)
+
+                            gene.start_codon = start_codon_line
+
+                            prev_line = "start_codon"
+                        elif line.split("\t")[2] == "stop_codon":
+
+                            stop_codon_line = GTF_File_Line()
+                            line_split = line.split("\t")
+                            stop_codon_line.load_from_line(line_split)
+
+                            gene.stop_codon = stop_codon_line
+
+                            genes.append(Gene(virus_id=gene.virus_id, CDS=gene.CDS, start_codon=gene.start_codon, stop_codon=gene.stop_codon))
+
+                            prev_line = "stop_codon"
+            except Exception as e:
+                print(e)
+                traceback.print_exc(file=sys.stdout)
+        return genes
+
+
+    @staticmethod
+    def load_file(url, map, genes):
         """
         :param url: Path to input SAM file
         :param map: Map of Virus id -> Virus Name
@@ -44,8 +104,11 @@ class Convertor:
                             for ambiguous_virus in ambiguous_viruses_column:
                                 if ambiguous_virus != "" and ambiguous_virus != "\n":
                                     v = ambiguous_virus.split(",")
-                                    ambiguous_viruses.append(Virus(virus_id=v[0], virus_name=map[v[0]]))
-                    sam_records.append(SamRecord(virus=Virus(virus_id=line_split[2], virus_name=map[line_split[2]]),
+                                    # genes to virus
+                                    g = [g for g in genes if g.virus_id == v[0]]
+                                    ambiguous_viruses.append(Virus(virus_id=v[0], virus_name=map[v[0]], genes=g))
+                    g = [g for g in genes if g.virus_id == line_split[2]]
+                    sam_records.append(SamRecord(virus=Virus(virus_id=line_split[2], virus_name=map[line_split[2]], genes=g),
                                                  ambiguous_viruses=ambiguous_viruses))
         except Exception as e:
             print(e)
