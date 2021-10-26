a94cd88a37a404b99d55bd0e8818139f73641592
Weliras <ptacek77711@seznam.cz>
2021-10-27 01:29:34 +0200

    SamRecord class

--- Classes/SamRecord.py
+++ Classes/SamRecord.py
@@ -2,7 +2,20 @@ from Classes.Virus import Virus
 
 
 class SamRecord:
-    def __init__(self, virus, ambiguous_viruses):
+    def __init__(self, virus=None, ambiguous_viruses=[], qname=None, flag=None, rname=None, pos=None, mapq=None, cigar=None,
+                 rnext=None, pnext=None, tlen=None, seq=None, qual=None):
+        self.QNAME = qname
+        self.FLAG = flag
+        self.RNAME = rname
+        self.POS = int(pos) if str.isnumeric(pos) else pos
+        self.MAPQ = mapq
+        self.CIGAR = cigar
+        self.RNEXT = rnext
+        self.PNEXT = pnext
+        self.TLEN = tlen
+        self.SEQ = seq
+        self.QUAL = qual
+
         self.virus = virus          # Virus id, Virus name, ...
         self.ambiguous_viruses = ambiguous_viruses  # List of Amb. Viruses
 
