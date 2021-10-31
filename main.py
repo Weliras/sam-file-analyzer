66901dea9dd12508265ae72099da518dcaf1d34e
Weliras <ptacek77711@seznam.cz>
2021-10-31 19:44:00 +0100

    Main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 
+from Classes.Gene import Gene
 from Classes.Virus import Virus
 from Classes.SamRecord import SamRecord
 from Classes.Convertor import Convertor
@@ -7,7 +8,6 @@ from Classes.Convertor import Convertor
 RNA_SAMPLE = "http://homel.vsb.cz/~vas218/files/viruses/viruses.filtered.sam"
 VIRUS_NAME = "http://homel.vsb.cz/~vas218/files/viruses/viral_id.csv"
 FEATURE_TO_GET_FROM_GTF = "cds"
-
 
 if __name__ == '__main__':
 
@@ -19,7 +19,7 @@ if __name__ == '__main__':
 
     # Load a file and mapping ids to names, list of Sequences
     # [VirusSeq, Count] -> [VirusName, Count] | VirusSeq : VirusName = N : 1
-    sam_records = Convertor.load_file(RNA_SAMPLE, map, genes)
+    sam_records = Convertor.load_sam_files(RNA_SAMPLE, map, genes)
 
     # for sam_record in sam_records:
     #    sam_record.virus.genes = [gene for gene in genes if gene.virus_id == sam_record.virus.virus_id]
@@ -28,15 +28,19 @@ if __name__ == '__main__':
     # sam_records.append(SamRecord(Virus("NC_002023.1", "Influenza_A_virus")))
     # sam_records.append(SamRecord(Virus("NC_002021.1", "Influenza_A_virus")))
 
-
     # getting list [ Virus, count_of_all, count_of_amb ] by attribute of Sequence
     virus_with_count = Convertor.get_seqs_with_count_grouped_by(sam_records, "virus_name")
 
+    # For each gene get % of mapped
+    Gene.write_to_file_genes_with_percents(genes=genes)
+
+    # For each virus get % of mapped
+    Gene.write_to_file_virus_with_percents(genes=genes)
+
     # sort it by count desc
-    virus_with_count.sort(key= lambda virus: virus[1], reverse=True)
+    virus_with_count.sort(key=lambda virus: virus[1], reverse=True)
 
     # Printing result VirusId\tVirusName\tCountOfAll\tCountOfAmb
     print("virus_id\tvirus_name\tcount_of_all\tcount_of_amb\tcount_of_IN\tcount_of_OUT\n")
     for item in virus_with_count:
         print(f"{item[0].virus_id}\t{item[0].virus_name}\t{item[1]}\t{item[2]}\t{item[3]}\t{item[4]}")
-
