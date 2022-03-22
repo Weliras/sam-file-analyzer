d43c516efa8ce1c139ce7e87131e37dc1ef1811c
Weliras <ptacek77711@seznam.cz>
2022-03-22 19:53:33 +0100

    Data Class for output

--- Core/Classes/DataForHTMLOutput.py
+++ Core/Classes/DataForHTMLOutput.py
@@ -9,7 +9,9 @@ class DataForHTMLOutput:
                  total_count_of_sam_records, count_of_sam_records_with_no_gtf, count_of_sam_records_filtered_out_with_n,
                  count_of_mapped_in_sam_records, count_of_candidates_for_blast, count_of_filtered_out_candidates,
                  sam_records_long_ends_starts, map_virus_id_name, count_of_not_mapped_records,
-                 find_sam_records_long_ends_starts, blast_is_set, best_candidates): # type: (list[list[Virus, int, int, int, int]],list[list[int, float, int]],dict[str, list[Gene]], dict[str, int], int, int, int, int, int, int, list[SamRecord], dict[str: str], int, bool, bool, list[SamRecord]) -> None
+                 find_sam_records_long_ends_starts, blast_is_set, best_candidates,
+                 default_json_v, defaul_json_g, default_output
+                 ): # type: (list[list[Virus, int, int, int, int]],list[list[int, float, int]],dict[str, list[Gene]], dict[str, int], int, int, int, int, int, int, list[SamRecord], dict[str: str], int, bool, bool, list[SamRecord], str, str, str) -> None
 
         self.virus_coverage = virus_coverage
         self.genes_coverage = genes_coverage
@@ -25,6 +27,9 @@ class DataForHTMLOutput:
         self.find_sam_records_long_ends_starts = find_sam_records_long_ends_starts
         self.sam_records_long_ends_starts = sam_records_long_ends_starts
         self.map_virus_id_name = map_virus_id_name
-        self.blast_is_set= blast_is_set
-        self.best_candidates= best_candidates
+        self.blast_is_set = blast_is_set
+        self.best_candidates = best_candidates
+        self.default_json_v = default_json_v
+        self.defaul_json_g = defaul_json_g
+        self.default_output = default_output
 
